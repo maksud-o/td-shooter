4368b7486a2f88980d2f5c0b5c8db11cbcd3cf91
maksud-o <privetyaarseniy@gmail.com>
2024-11-10 20:42:53 +0300

    add level change on trigger

---
+++ Assets/Scripts/Game/LevelCompletionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TDS.Architecture;
+using TDS.Architecture.Services;
+using UnityEngine;
+
+namespace TDS.Game
+{
+    public class LevelCompletionHandler : MonoBehaviour
+    {
+        [SerializeField] private string _nextLevelName;
+        [SerializeField] private TriggerObserver _nextLevelTrigger;
+
+        private void OnEnable()
+        {
+            _nextLevelTrigger.OnEnter += TriggerEnter2DCallback;
+        }
+
+        private void OnDisable()
+        {
+            _nextLevelTrigger.OnEnter -= TriggerEnter2DCallback;
+        }
+        
+        private void TriggerEnter2DCallback(Collider2D collision)
+        {
+            var loader = ServiceLocator.Instance.Get<SceneLoaderService>();
+            loader.Load(_nextLevelName);
+        }
+    }
+}
